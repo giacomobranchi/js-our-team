6c620ebd0dd243f3e85e052ad0e3ae4f6639c77f
giacomobranchi <branchigiacomo28@gmail.com>
2023-09-06 15:20:27 +0200

    add milestone 0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,59 @@
 // Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
 // MILESTONE 0:
 // Creare l’array di oggetti con le informazioni fornite.
+/* Dati:
+Wayne Barnett
+Founder & CEO	
+wayne-barnett-founder-ceo.jpg
+Angela Caroll	
+Chief Editor	
+angela-caroll-chief-editor.jpg
+Walter Gordon
+Office Manager
+walter-gordon-office-manager.jpg
+Angela Lopez
+Social Media Manager	
+angela-lopez-social-media-manager.jpg
+Scott Estrada	
+Developer	
+scott-estrada-developer.jpg
+Barbara Ramos	
+Graphic Designer	
+barbara-ramos-graphic-designer.jpg */
 
+const members = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        photo: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        photo: 'angela-caroll-chief-editor.jpg.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        photo: 'walter-gordon-office-manager.jpgg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        photo: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        photo: 'scott-estrada-developer.jpg'
+    }
+    ,
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        photo: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
 
 
 
